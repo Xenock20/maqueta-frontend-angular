e7579a7d4b5b5dbd1344ceedca1bc5b897bd9930
Xenock20 <juliosaboredo@gmail.com>
2023-02-13 14:10:30 -0300

    Fix style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/page/home/home.component';
 import { Pagina404Component } from './components/page/pagina404/pagina404.component';
+import { EditpageComponent } from './components/page/editpage/editpage.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'home', component:HomeComponent},
   {path:'login', component:LoginComponent},
+  {path:'edit', component:EditpageComponent},
   {path: '**', component: Pagina404Component}
 ];
 
